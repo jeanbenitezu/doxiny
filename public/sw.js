c804feb01e25f5dc2ac8169014cbb9cb51d756af
jeanbenitezu <jcbenitez@univision.net>
2026-03-20 20:46:54 -0500

    v1774057606928

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
 
 // Generate cache version based on current timestamp for automatic updates
 const CACHE_VERSION = '1.0.0';
-const BUILD_TIMESTAMP = 1774056270339; // Built on: 2026-03-19T00:58:30.397Z
+const BUILD_TIMESTAMP = 1774057606928; // Built on: 2026-03-19T00:58:30.397Z
 const CACHE_NAME = `number-puzzle-v${CACHE_VERSION}-${BUILD_TIMESTAMP}`;
 
 const urlsToCache = [
